3623f2bcd5918e804b8a3412760bcc2067b5ff1b
Shubham Swarnakar <shubham2swarnakar@gmail.com>
2025-06-19 02:28:29 +0530

    basic search engine

--- 1_Prompt/prompt.py
+++ 1_Prompt/prompt.py
@@ -1,1 +1,18 @@
-from langchain import PromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+model = ChatOpenAI()
+
+st.header("Research Tool")
+
+user_input = st.text_input("Enter your research question:")
+
+if st.button("summarize"):
+    result = model.invoke(user_input)
+    st.write(result.content)
+
